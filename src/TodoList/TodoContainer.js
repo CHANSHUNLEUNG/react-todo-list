15982261688fa0e8eaf0c23bebe8a61184084cde
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-22 16:56:32 +0800

    move todolist state and updateTodoList method from TodoForm.js to TodoContainer.js

--- src/TodoList/TodoContainer.js
+++ src/TodoList/TodoContainer.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react'
 import { Layout } from 'antd';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import axios from 'axios';
+
 const { Header, Footer, Content } = Layout;
 
 export default class TodoContainer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.updateTodoList = this.updateTodoList.bind(this);
+
+        this.state = {
+            todoList: [],
+        }
+    }
+
+    componentDidMount() {
+        this.updateTodoList();
+    }
+
+    updateTodoList() {
+        const TODO_LIST_URL = "https://5e9ec500fb467500166c4658.mockapi.io/todos";
+        axios.get(TODO_LIST_URL).then(response => {
+            console.log(response.data);
+            this.setState({
+                todoList: response.data,
+            })
+        })
+    }
+
     render() {
         return (
             <Layout>
                 <Header>TodoList</Header>
                 <Content>
-                    <TodoList />
+                    <TodoList todoList={this.state.todoList} />
                 </Content>
                 <Footer>
-                    <TodoForm />
+                    <TodoForm updateTodoList={this.updateTodoList}/>
                 </Footer>
             </Layout>
         )
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -2,28 +2,10 @@ import React, { Component } from 'react'
 import axios from 'axios';
 
 export default class TodoList extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             todoList: [],
-        }
-    }
-
-    componentDidMount(){
-        const TODO_LIST_URL = "https://5e9ec500fb467500166c4658.mockapi.io/todos";
-        axios.get(TODO_LIST_URL).then(response => {
-            console.log(response.data);
-            this.setState({
-                todoList: response.data,
-            })
-        })
-    }
-    
     render() {
         return (
             <div>
-                {this.state.todoList.map((oneTodo) => {
+                {this.props.todoList.map((oneTodo) => {
                     console.log(oneTodo);
                     console.log("next");
                     return "";
